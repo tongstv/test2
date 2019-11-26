ec0231252b986c0baaeafc6544b816962a985989
Tongstv <tongstv@gmail.com>
2019-11-26 13:27:13 +0700

    nang cap 1999

--- m/includes/dangnhap.php
+++ m/includes/dangnhap.php
@@ -1,1 +1,5 @@
 <?php $web_title='Đăng nhập';$nav_select=$act;if($mtv_id>0){include('includes/canhan.php');}else{$main_content=$func->str_template('template/dangnhap.html',array());} 
+
+<?php
+echo 1;
+?>
--- m/includes/error.php
+++ m/includes/error.php
@@ -1,2 +1,2 @@
 <?php
-$main_content = '<h3 align="center">Không tồn tại dữ liệu bạn đang tìm kiếm</h3>';
+$main_content = '111<h3 align="center">Không tồn tại dữ liệu bạn đang tìm kiếm</h3>';
